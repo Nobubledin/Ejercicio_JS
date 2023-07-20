35e7a2f2273883fd1dfaf3c6119324d24c5ca4be
Oscar Iglesias <oscariglesias2010@gmail.com>
2023-07-20 19:46:55 +0200

    Bucle while funcional

--- ejercicio1.js
+++ ejercicio1.js
@@ -7,21 +7,14 @@ let redes = ["Github", "https://github.com/"]
 const afirmativo = "Sí";
 const negativo = "No";
 
+let respuesta = prompt("¿Estás buscando trabajo?");
+
+while(respuesta != afirmativo && respuesta != negativo) {
+    respuesta = prompt("¿Estás buscando trabajo? Sí / No");
+}
+
 if(respuesta == afirmativo) {
     alert("Este usuario está buscando trabajo");
 } else if(respuesta == negativo) {
     alert("Este usuario no está buscando trabajo");
-} else {
-    while(respuesta != afirmativo) {
-        let respuesta = prompt("¿Estás buscando trabajo? Sí / No");
-    }    
 }
-
-
-
-/*let numero = 1;
-
-while(numero != 10) {
-    console.log(numero);
-    numero = numero + 1;
-}*/
